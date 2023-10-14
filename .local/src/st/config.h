17c11e62c45a570200d881a191ffd41fbbea09e4
pseudominimalista <dfern3321@gmail.com>
2023-10-14 14:13:08 -0300

    Nova organização

--- .local/src/st/config.h
+++ .local/src/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono:pixelsize=10:antialias=true:autohint=true";
+static char *font = "JetBrainsMono:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /* bg opacity */
